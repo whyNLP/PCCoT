7d8d7d2325de6f5f66978b7325672de57844856d
whywhy-a6000 <wuhy1@shanghaitech.edu.cn>
2025-03-23 10:34:19 +0800

    fix: wandb upload

--- models/__init__.py
+++ models/__init__.py
@@ -3,8 +3,12 @@ from .modeling_llama import PCoTLlamaForCausalLM
 from .pcot_arguments import PCoTArguments
 from .data_processor import COTDataProcessor
 from .wandb_callback import CustomWandbCallback
+from transformers.integrations import INTEGRATION_TO_CALLBACK
 
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoModelForTokenClassification
 
 AutoConfig.register("pcot-llama", PCoTLlamaConfig)
 AutoModelForCausalLM.register(PCoTLlamaConfig, PCoTLlamaForCausalLM)
+
+# register to transformer callback
+INTEGRATION_TO_CALLBACK["wandb"] = CustomWandbCallback
