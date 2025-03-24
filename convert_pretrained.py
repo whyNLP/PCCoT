016be9c4e68a69d483e06ae776e916a06e5c361a
whywhy-a6000 <wuhy1@shanghaitech.edu.cn>
2025-03-24 19:52:03 +0800

    feat: safe convert model

--- convert_pretrained.py
+++ convert_pretrained.py
@@ -1,9 +1,19 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 import models
 
 def main():
-    model = models.PCoTLlamaForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-intermediate-step-1195k-token-2.5T")
-    model.save_pretrained("outputs/TinyLlama-2.5T-pcot")
+    model = models.PCoTLlamaForCausalLM.from_pretrained("JackFram/llama-160m")
+
+    # check the embeddings to avoid zero lines
+    embedding = model.model.get_input_embeddings()
+    for i in range(embedding.weight.size(0)):
+        line = embedding.weight[i]
+        if torch.allclose(line, torch.zeros_like(line)):
+            print("Found zero line", i)
+            embedding.weight.data[i] = torch.randn_like(line) * 1e-5
+
+    model.save_pretrained("outputs/JackFram--llama-160m-pcot")
 
 if __name__ == "__main__":
     main()
